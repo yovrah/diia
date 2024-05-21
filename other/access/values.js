2dd4d5dfe0e759673a8240010653498254789367
yovrah <75204500+yovrah@users.noreply.github.com>
2024-05-21 16:47:17 +0300

    Update values.js

--- other/access/values.js
+++ other/access/values.js
@@ -3,9 +3,9 @@ var isRightsEnabled = true; // Права
 var isCovidCertificateEnabled = true; // COVID-сертификат
 
 // Общее
-var birthdate = "01.01.2001"; // Дата рождения
-var fullname = "Фамилия Имя Отчество" // ФИО
-var name = "Имя" // Имя
+var birthdate = "22.12.2005"; // Дата рождения
+var fullname = "Ангелов Владислав Юрійович" // ФИО
+var name = "Владислав" // Имя
 
 // Паспорт
 var passport_id = "123456789" // Номер паспорта
@@ -14,7 +14,7 @@ var passport_id = "123456789" // Номер паспорта
 var kpp_id = "0123456789" // ИНН
 
 // Права
-var rights_valid_until = "01.01.2022" // Права "Дійсні до"
+var rights_valid_until = "18.01.2034" // Права "Дійсні до"
 var rights_categories = "A, B" // Права "Категорії"
 var rights_tsc = "ТСЦ 8631" // Права "Видав
 var rights_id = "KBE968639" // Права "Серія та номер""
